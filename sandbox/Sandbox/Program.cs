7b91d699cf251004c65267a340e754e9f1934bb7
Lebohang Sebata <lebohangsebata20@gmail.com>
2025-05-23 15:49:35 +0200

    Exercise 1 Done

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Lebo!");
+        int x = 78;
+        int y = 34;
+
+        if (x > y){
+            Console.WriteLine("x is greater than y");
+        }
+        else if (x < y){
+            Console.WriteLine("x is less than y");
+        }
+        else{
+            Console.WriteLine("x is equal to y");
+        }
     }
 }
