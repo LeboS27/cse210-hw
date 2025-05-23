92d5b70ba911d8f4c6e1d9c65f24cd856cfb62fa
Lebohang Sebata <lebohangsebata20@gmail.com>
2025-05-23 16:01:26 +0200

    Exercise 2 Done

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter the mark you got from the exam: ");
+        int mark = int.Parse(Console.ReadLine());
+        if (mark >= 90)
+        {
+            Console.WriteLine("A");
+        }
+        else if (mark >= 80)
+        {
+            Console.WriteLine("B");
+        }
+        else if (mark >= 70)
+        {
+            Console.WriteLine("C");
+        }
+        else if (mark >= 60)
+        {
+            Console.WriteLine("D");
+        }
+        else if (mark < 60)
+        {
+            Console.WriteLine("F");
+        }
+        else
+        {
+            Console.WriteLine("Invalid Mark");
+        }
     }
 }
