333b95d004f9f9a26b29037f9ea902dc37935588
Lebohang Sebata <lebohangsebata20@gmail.com>
2025-05-25 22:38:24 +0200

    Created Resume.cs & Job.cs

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        Job job2 = new Job();
+        job1._company = "Microsoft";
+        job1._jobtitle = "Software Engineer";
+        job1._startyear = "2019";
+        job1._endyear = "2022";
+        job2._company = "Apple";
+        job2._jobtitle = "Manager";
+        job2._startyear = "2022";
+        job2._endyear = "2023";
+        //job1.Displayjob();
+        //job2.Displayjob();
+        Resume myresume = new Resume();
+        myresume._name = "Lebohang Sebata";
+        myresume._jobs.Add(job1);
+        myresume._jobs.Add(job2);
+        myresume.Display();
     }
 }
