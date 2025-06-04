811c9ba8d3087f06635604f42a14a6a33523a4ec
Lebohang Sebata <lebohangsebata20@gmail.com>
2025-06-04 12:02:25 +0200

    Practice

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,17 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        int x = 78;
-        int y = 34;
+        // You can create and use Person or Student objects here if needed.
+        Student student = new Student("Brigham", "234");
+        string name = student.GetName();
+        string number = student.GetNumber();
+        Console.WriteLine(name);
+        Console.WriteLine(number);
+    }
+}
 
-        if (x > y){
-            Console.WriteLine("x is greater than y");
-        }
-        else if (x < y){
-            Console.WriteLine("x is less than y");
-        }
-        else{
-            Console.WriteLine("x is equal to y");
-        }
+// a parent class called Person
+public class Person
+{
+    private string _name;
+
+    public Person(string name)
+    {
+        _name = name;
+    }
+
+    public string GetName()
+    {
+        return _name;
+    }
+}
+
+// a child class called Student
+public class Student : Person
+{
+    private string _number;
+
+    // calling the parent constructor using "base"!
+    public Student(string name, string number) : base(name)
+    {
+        _number = number;
+    }
+
+    public string GetNumber()
+    {
+        return _number;
     }
 }
