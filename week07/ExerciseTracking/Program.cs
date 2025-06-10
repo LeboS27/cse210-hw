1d666a0a26f053395f03c36d28ab3605050d1575
Lebohang Sebata <lebohangsebata20@gmail.com>
2025-06-10 14:42:58 +0200

    Exercise Tracking Done

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        // Create activities
+        var activities = new List<Activity>
+        {
+            new Running(new DateTime(2022, 11, 3), 30, 3.0),
+            new Cycling(new DateTime(2022, 11, 3), 30, 9.7),
+            new Swimming(new DateTime(2022, 11, 3), 30, 20)
+        };
+
+        // Display summaries
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
