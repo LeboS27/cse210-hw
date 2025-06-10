b58cae2d5bb2e18156b7b28dc2b95461dc42a057
Lebohang Sebata <lebohangsebata20@gmail.com>
2025-06-10 12:43:26 +0200

    Shapes Done & Dusted

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Shape Circle = new Circle(6, "Blue");
+        Shape Rectangle = new Rectangle(3, 6, "Green");
+        Shape Square = new Square(8, "Purple");
+
+        //Console.WriteLine($"Circle Area: {Circle.GetArea()} Color: {Circle.GetColor()}");
+        //Console.WriteLine($"Rectangle Area: {Rectangle.GetArea()} Color: {Rectangle.GetColor()}");
+        //Console.WriteLine($"Square Area: {Square.GetArea()} Color: {Square.GetColor()}");
+
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(Circle);
+        shapes.Add(Rectangle);
+        shapes.Add(Square);
+
+        foreach (Shape shapex in shapes)
+        {
+            Console.WriteLine($"Shape Area: {shapex.GetArea()}");
+        }
+
     }
 }
